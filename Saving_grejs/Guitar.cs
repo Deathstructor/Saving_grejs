ed8fc5096e0693a749f4ca6dbcaf99ce0687c5c7
Paul Chastain <paulchast1@gmail.com>
2022-10-19 13:25:16 +0200

    class + questions done

---
+++ Saving_grejs/Guitar.cs
@@ -0,0 +1,6 @@
+public class Guitar
+{
+    public int stringAmount { get; set; }
+    public string? tuning { get; set; }
+    public string? color { get; set; }
+}
--- Saving_grejs/Program.cs
+++ Saving_grejs/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+Guitar guitar = new Guitar();
+int sa;
+
+Console.WriteLine("How many string would you like to have on your guitar?");
+Console.WriteLine("You can choose between 6, 7, 8 or 9 strings:");
+string? stringAmountInput = Console.ReadLine();
+bool validAnswer = int.TryParse(stringAmountInput, out sa);
+guitar.stringAmount = sa;
+Console.Clear();
+
+
+Console.WriteLine("Would you like to use a drop-tuning? (Y/N)");
+string? yesNo = Console.ReadLine();
+ConsoleKey key = Console.ReadKey().Key;
+while (key != ConsoleKey.Y || key != ConsoleKey.N)
+{
+    Console.Clear();
+    
+    if (key == ConsoleKey.Y)
+    {
+        Console.WriteLine("What drop tuning would you like to use?");
+        Console.WriteLine("You can choose between Drop D, Drop C, Drop B and Drop A:");
+        guitar.tuning = Console.ReadLine();
+    }
+    else if (key == ConsoleKey.N)
+    {
+        Console.WriteLine("What standard tuning would you like to use?");
+        Console.WriteLine("You can choose between E, D#, D, C#, C, B, A#, A and F#:");
+        guitar.tuning = Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("Please press Y (yes) or N (no) to continue.");
+        Console.WriteLine();
+    }
+}
+
+
+
+Console.WriteLine("What color would you like to have on your guitar?");
+guitar.color = Console.ReadLine();
+
+
+
+Console.ReadLine();

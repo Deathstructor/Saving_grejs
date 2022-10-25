8c2983dfb66a399bcc80279243e5765a17683e4a
Paul Chastain <paulchast1@gmail.com>
2022-10-25 12:58:11 +0200

    fixed serializing

--- Saving_grejs/Program.cs
+++ Saving_grejs/Program.cs
@@ -1,47 +1,62 @@
 ﻿using System.IO;
+using System.Text.Json;
 
-Guitar guitar = new Guitar();
+// string read = File.ReadAllText("guitar.json");
+// Guitar readGuitar = JsonSerializer.Deserialize<Guitar>(read);
+
 int sa;
+string tune;
+string col;
+
 
 Console.WriteLine("How many string would you like to have on your guitar?");
 Console.WriteLine("You can choose between 6, 7, 8 or 9 strings:");
 string? stringAmountInput = Console.ReadLine();
 bool validAnswer = int.TryParse(stringAmountInput, out sa);
-guitar.stringAmount = sa;
 Console.Clear();
 
 
 Console.WriteLine("Would you like to use a drop-tuning? (Y/N)");
-string? yesNo = Console.ReadLine();
 ConsoleKey key = Console.ReadKey().Key;
-while (key != ConsoleKey.Y || key != ConsoleKey.N)
+Console.Clear();
+  
+if (key == ConsoleKey.Y)
 {
-    Console.Clear();
-    
-    if (key == ConsoleKey.Y)
-    {
-        Console.WriteLine("What drop tuning would you like to use?");
-        Console.WriteLine("You can choose between Drop D, Drop C, Drop B and Drop A:");
-        guitar.tuning = Console.ReadLine();
-    }
-    else if (key == ConsoleKey.N)
-    {
-        Console.WriteLine("What standard tuning would you like to use?");
-        Console.WriteLine("You can choose between E, D#, D, C#, C, B, A#, A and F#:");
-        guitar.tuning = Console.ReadLine();
-    }
-    else
-    {
-        Console.WriteLine("Please press Y (yes) or N (no) to continue.");
-        Console.WriteLine();
-    }
+    Console.WriteLine("What drop tuning would you like to use?");
+    Console.WriteLine("You can choose between Drop D, Drop C, Drop B and Drop A:");
+    tune = Console.ReadLine();
 }
+else if (key == ConsoleKey.N)
+{
+    Console.WriteLine("What standard tuning would you like to use?");
+    Console.WriteLine("You can choose between E, D#, D, C#, C, B, A#, A and F#:");
+    tune = Console.ReadLine();
+}
+else
+{
+    tune = "null";
+}
+Console.Clear();
 
 
 
 Console.WriteLine("What color would you like to have on your guitar?");
-guitar.color = Console.ReadLine();
+col = Console.ReadLine();
+Console.Clear();
 
+Guitar guitar = new Guitar()
+{
+    stringAmount = sa,
+    tuning = tune,
+    color = col
+};
+
+string serialize = JsonSerializer.Serialize<Guitar>(guitar);
+File.WriteAllText("guitar.json", serialize);
+
+
+
+Console.WriteLine("You have successfully customized your guitar. Press any ENTER to exit.");
 
 
 Console.ReadLine();
